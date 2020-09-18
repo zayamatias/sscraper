f295d1140063b287a2ffba68f3a6e7fa36c69416
zayamatias <32246591+zayamatias@users.noreply.github.com>
2020-09-18 10:10:20 +0100

    Create tables.sql
    Tables that need to be created in your database

---
+++ tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE 'filehashes' (
+  'file' varchar(300) NOT NULL,
+  'SHA1' varchar(100) DEFAULT NULL,
+  'MD5' varchar(100) DEFAULT NULL,
+  'CRC' varchar(100) DEFAULT NULL,
+  PRIMARY KEY ('file'),
+  KEY 'filehashes_file_IDX' ('file') USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+
+
+CREATE TABLE 'hashes' (
+  'hash' char(40) NOT NULL,
+  'response' longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  'created_at' timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY ('hash')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
